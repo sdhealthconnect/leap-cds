bf3c411e1ee8db64a56ae060331675c391523213
Mohammad Jafari <jafarim@gmail.com>
2019-11-16 11:44:45 -0800

    validate hook name

--- test/controllers/patient-consent-consult.test.js
+++ test/controllers/patient-consent-consult.test.js
@@ -4,7 +4,7 @@ const { app } = require("../../app");
 const HOOK_ENDPOINT = "/cds-services/patient-consent-consult";
 
 it("should return 400 on bad query", async () => {
-  expect.assertions(2);
+  expect.assertions(4);
 
   let res = await request(app)
     .post(HOOK_ENDPOINT)
@@ -12,4 +12,29 @@ it("should return 400 on bad query", async () => {
     .send({});
   expect(res.status).toEqual(400);
   expect(res.body).toMatchObject({ error: "bad_request" });
+
+  res = await request(app)
+    .post(HOOK_ENDPOINT)
+    .set("Accept", "application/json")
+    .send({
+      hookInstance: "12342"
+    });
+  expect(res.status).toEqual(400);
+  expect(res.body).toMatchObject({ error: "bad_request" });
 });
+
+it("should return 400 on wrong hook name", async () => {
+  expect.assertions(3);
+
+  const res = await request(app)
+    .post(HOOK_ENDPOINT)
+    .set("Accept", "application/json")
+    .send({
+      hook: "wrongName",
+      hookInstance: "12342",
+      context: {}
+    });
+  expect(res.status).toEqual(400);
+  expect(res.body).toMatchObject({ error: "bad_request" });
+  expect(res.body.errorMessage).toMatch("patient-consent-consult");
+});  
