c4ae8f9416557f39e01f14d54b74728951fe66ae
Mohammad Jafari <jafarim@gmail.com>
2020-02-14 15:29:22 -0800

    unit test coverage for multiple patient identifiers.

--- test/lib/consent-discovery.test.js
+++ test/lib/consent-discovery.test.js
@@ -1,4 +1,4 @@
-const _ = require("lodash");
+const nock = require("nock");
 const { fetchConsents } = require("../../lib/consent-discovery");
 
 const CONSENT = require("../fixtures/consents/consent-boris.json");
@@ -9,24 +9,49 @@ const {
   MOCK_FHIR_SERVERS
 } = require("../common/setup-mock-consent-servers");
 
+afterEach(() => {
+  nock.cleanAll();
+});
+
 it("make sure there is at least one FHIR Consent Server", async () => {
   expect(MOCK_FHIR_SERVERS.length).toBeGreaterThan(0);
 });
 
-it("should return an array of consents from all servers", async () => {
-  expect.assertions(2);
-
-  setupMockPatient({ system: "ssn", value: "111111111" });
-  setupMockConsent("", CONSENT);
-
-  let consents = await fetchConsents([{ system: "ssn", value: "111111111" }]);
-  expect(consents).toHaveLength(1);
+it("should return an array of consents from all servers with consent scope", async () => {
+  expect.assertions(1);
 
   setupMockPatient({ system: "ssn", value: "111111111" });
   setupMockConsent("patient-privacy", CONSENT);
 
-  consents = await fetchConsents(
+  const consents = await fetchConsents(
     [{ system: "ssn", value: "111111111" }],
+    "patient-privacy"
+  );
+  expect(consents).toHaveLength(1);
+});
+
+it("should return an array of consents from all servers without consent scope", async () => {
+  expect.assertions(1);
+
+  setupMockPatient({ system: "ssn", value: "111111111" });
+  setupMockConsent("", CONSENT);
+
+  const consents = await fetchConsents([{ system: "ssn", value: "111111111" }]);
+  expect(consents).toHaveLength(1);
+});
+
+it("should return an array of consents from all servers based on multiple patient identifiers", async () => {
+  expect.assertions(1);
+
+  setupMockPatient({ system: "ssn", value: "111111111" }, 0);
+  setupMockPatient({ system: "other-system", value: "22222222" }, 1);
+  setupMockConsent("patient-privacy", CONSENT, 0);
+
+  const consents = await fetchConsents(
+    [
+      { system: "other-system", value: "22222222" },
+      { system: "ssn", value: "111111111" }
+    ],
     "patient-privacy"
   );
   expect(consents).toHaveLength(1);
