bf21cff7bc1fd80db6fc472f14624877bfbaf98b
Mohammad Jafari <jafarim@gmail.com>
2023-03-09 00:32:28 -0800

    add redaction service.

---
+++ test/lib/redacter.test.js
@@ -0,0 +1,40 @@
+const _ = require("lodash");
+const { processBundle } = require("../../lib/labeling/labeler");
+const { maybeRedactBundle } = require("../../lib/redacter");
+
+const RESTRICTED_OBSERVATION = require("../fixtures/observations/observations-ketamine.json");
+const NON_RESTRICTED_OBSERVATION = require("../fixtures/observations/observation-bacteria.json");
+const BUNDLE = require("../fixtures/empty-bundle.json");
+const OBLIGATIONS = [
+  {
+    id: {
+      system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+      code: "REDACT"
+    },
+    parameters: {
+      codes: [
+        {
+          system: "http://terminology.hl7.org/CodeSystem/v3-Confidentiality",
+          code: "R"
+        }
+      ]
+    }
+  }
+];
+
+it("correctly redacts a bundle of resources", async () => {
+  const bundleOfObservations = _.cloneDeep(BUNDLE);
+  bundleOfObservations.entry = [
+    { fullUrl: "1", resource: RESTRICTED_OBSERVATION },
+    { fullUrl: "2", resource: NON_RESTRICTED_OBSERVATION }
+  ];
+  bundleOfObservations.total = 2;
+
+  const labeledBundle = processBundle(bundleOfObservations);
+  console.log(JSON.stringify(labeledBundle, null, 2));
+
+  const modifiedBundle = maybeRedactBundle(OBLIGATIONS, labeledBundle);
+
+  expect(modifiedBundle.total).toBe(1);
+  expect(modifiedBundle.entry).toHaveLength(1);
+});
