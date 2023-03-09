909481991cef76ec39bd842c082424bef56e1d6d
Mohammad Jafari <jafarim@gmail.com>
2023-03-08 19:46:11 -0800

    bundle labeler

--- test/lib/labeling.test.js
+++ test/lib/labeling.test.js
@@ -1,7 +1,11 @@
 const _ = require("lodash");
-const { processResource } = require("../../lib/labeling/labeler");
+const {
+  processResource,
+  processBundle
+} = require("../../lib/labeling/labeler");
 
 const OBSERVATION = require("../fixtures/observations/observations-ketamine.json");
+const BUNDLE = require("../fixtures/empty-bundle.json");
 
 it("correctly labels an unlabled resource based on sensitivity rules", async () => {
   const labeledObservation = processResource(OBSERVATION);
@@ -60,6 +64,31 @@ it("correctly adds labels to a resource with existing labels based on sensitivit
       expect.objectContaining({
         system: "http://terminology.hl7.org/CodeSystem/v3-ObservationValue",
         code: "HRELIABLE"
+      })
+    ])
+  );
+});
+
+it.only("correctly labels a bundle of resource based on sensitivity rules", async () => {
+  const bundleOfObservations = _.cloneDeep(BUNDLE);
+  bundleOfObservations.entry = [
+    { fullUrl: "1", resource: OBSERVATION },
+    { fullUrl: "2", resource: OBSERVATION }
+  ];
+  const labeledBundle = processBundle(bundleOfObservations);
+  expect(labeledBundle.entry[0].resource.meta?.security).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+        code: "ETH"
+      })
+    ])
+  );
+  expect(labeledBundle.entry[1].resource.meta?.security).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+        code: "ETH"
       })
     ])
   );
